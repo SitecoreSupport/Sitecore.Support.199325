825028f8b1defdb6fdafdb0df4166dca4e117b45
sc-ido <ido@sitecore.net>
2017-12-20 17:45:03 +0200

    Original functionality

---
+++ src/Sitecore.Support.199325/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.199325")]
+[assembly: AssemblyProduct("Sitecore.Support.199325")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Characters for variations in the Experience Editor are reversed")]
+
